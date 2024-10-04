abd19dbaf24df06150a13eea79e85fdaf325f417
PimentelJustin <pimentel.justin.espiritu@gmail.com>
2024-10-04 12:24:29 +0800

    Activity

---
+++ 04BasicInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string Nickname, CnY, School;
+	
+	cout << "Nickname: ";
+	cin >> Nickname;
+	cout << "Course-Year Level: ";
+	cin >> CnY;
+	cout << "School: ";
+	cin >> School;
+	return 0;
+}
